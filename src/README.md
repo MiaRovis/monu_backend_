52caae79425dea37c747cfa15c411226dd6af120
Mia Rovis <mia.rovis@gmail.com>
2023-08-18 19:58:03 +0200

    Initial commit

---
+++ src/README.md
@@ -0,0 +1,1 @@
+Backend za Monu aplikaciju
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+console.log(req.query)
+res.send('hello u browser')
+console.log("hello u konzolu")
+
+})
+
+// `
+
+app.listen(port, () => console.log( `slusam na portu ${port}`))
