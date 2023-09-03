6c146976565758fe4703449bd3ea66bca6167295
Mia Rovis <mia.rovis@gmail.com>
2023-09-03 21:18:11 +0200

    vercel pt.2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ import cors from "cors";
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({
+    origin:"*",
+    methods: ["POST","GET","DELETE"],
+    credentials: true
+}));
+
 app.use(express.json());
 
 
